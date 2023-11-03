137c952c0df3587773a89cae25a82db30af4d981
Mats_Prodato <148777070+MBPD2023@users.noreply.github.com>
2023-11-03 10:30:13 +0100

    Update streamlit_app_pt2.py
    write df into page

--- streamlit_app_pt2.py
+++ streamlit_app_pt2.py
@@ -13,6 +13,8 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
 
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_catalog)
+#Turn Data into Dataframe
+df = pd.DataFrame(my_catalog
 
+#write df into page to check
+streamlit.write(df)
