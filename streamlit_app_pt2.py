84e64e6802d84b15d6a74df6698116909a96861e
Mats_Prodato <148777070+MBPD2023@users.noreply.github.com>
2023-11-03 10:23:12 +0100

    Update streamlit_app_pt2.py
    added () to cursor

--- streamlit_app_pt2.py
+++ streamlit_app_pt2.py
@@ -8,7 +8,7 @@ streamlit.title("Zenas Amazing Athleisure Catalog")
 
 #connect to snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor
+my_cur = my_cnx.cursor()
 
 my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
